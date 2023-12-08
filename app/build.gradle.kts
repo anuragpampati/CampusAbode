b9c7913e513fc7fe652cd4583407a272d0841ad6
saimanojratnakaram16 <saimanoj.ratnakaram16@gmail.com>
2023-12-07 19:20:01 -0500

    Authentication added to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -51,9 +51,10 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.firebase:firebase-firestore:24.9.1")
-    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.squareup.picasso:picasso:2.71828")
     implementation("com.google.firebase:firebase-storage:20.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/campusabode/Login.kt
@@ -0,0 +1,72 @@
+package com.example.campusabode
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentContainerView
+import com.example.campusabode.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.logging.Logger
+
+class Login : AppCompatActivity(), ForgotPasswordFragment.Parent {
+    private lateinit var binding:ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth= FirebaseAuth.getInstance()
+        val notRegistered = findViewById<TextView>(R.id.notRegistered)
+        val forgotPassword = findViewById<TextView>(R.id.forgotPass)
+        val loginForm = findViewById<LinearLayout>(R.id.loginForm)
+        val forgotPassFragment = findViewById<FragmentContainerView>(R.id.forgotPassFragment)
+
+        forgotPassword.setOnClickListener{
+            val t =Logger.getLogger("test Forgot passcode")
+            t.warning("****************************")
+            loginForm.visibility = View.GONE
+            forgotPassFragment.visibility = View.VISIBLE
+            val fpFragment = ForgotPasswordFragment()
+            fpFragment.setParent(this)
+            supportFragmentManager.beginTransaction().replace(R.id.forgotPassFragment, fpFragment).commit()
+
+        }
+        notRegistered.setOnClickListener{
+            val t =Logger.getLogger("REgister")
+            t.warning("########################")
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+        binding.button.setOnClickListener{
+            val email = binding.emailEt.text.toString()
+            val password = binding.passET.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }else{
+                Toast.makeText(this, "Please enter all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(firebaseAuth.currentUser !=null){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
